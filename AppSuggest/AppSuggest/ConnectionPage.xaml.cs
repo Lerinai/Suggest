5b5e1f699e59e9193862b490e1791f9dba1d3b41
Lerinai <mantes.bastien@stagiairesifa.fr>
2019-06-28 09:15:48 +0200

    initial commit

---
+++ AppSuggest/AppSuggest/ConnectionPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppSuggest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConnectionPage : ContentPage
+    {
+        public ConnectionPage()
+        {
+            InitializeComponent();
+        }
+
+        async void EnterClicker(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage()); // Faire lien pour la page initiale avec les verif etc
+        }
+    }
+}
