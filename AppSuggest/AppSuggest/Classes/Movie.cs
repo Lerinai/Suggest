dfe7a929f28364b4d8d277257469bc8076ea6fd8
FlowerSushi <luc.vincent531@hotmail.Fr>
2019-07-01 16:48:45 +0200

    Display API results list

--- AppSuggest/AppSuggest/Classes/Movie.cs
+++ AppSuggest/AppSuggest/Classes/Movie.cs
@@ -9,9 +9,18 @@ namespace AppSuggest
     {
         [JsonProperty("id")]
         public int ID { get; private set; }
+
         [JsonProperty("title")]
         public string Title { get; private set; }
+
         [JsonProperty("release_date")]
         public string ReleaseDate { get; private set; }
+
+        public Movie(int id, string titre, string rd)
+        {
+            ID = id;
+            Title = titre;
+            ReleaseDate = rd;
+        }
     }
 }
