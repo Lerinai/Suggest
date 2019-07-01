502a2e6e081f010a48972a1cfef8bfb0bedd1bf2
CapiCode <giovannikce@gmail.com>
2019-07-01 10:15:07 +0200

    Mise à jour drawable

--- AppSuggest/AppSuggest.Android/MainActivity.cs
+++ AppSuggest/AppSuggest.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AppSuggest.Droid
 {
-    [Activity(Label = "AppSuggest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,16 +18,18 @@ namespace AppSuggest.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    [Activity(Label = "Suggest", Icon = "@drawable/movie2", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
         }
     }
 }
