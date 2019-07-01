9719aee7704a879fc0676d24499a31cc59b8c3b4
FlowerSushi <luc.vincent531@hotmail.Fr>
2019-07-01 15:29:28 +0200

    API request added

---
+++ AppSuggest/AppSuggest/Classes/APIResult.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSuggest
+{
+    public class MoviesList
+    {
+        [JsonProperty("results")]
+        public List<Movie> ResultsArray { get; set; }
+    }
+
+    public class RestService
+    {
+        HttpClient _client;
+
+        public RestService()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<List<Movie>> GetMovieDataAsync(string uri)
+        {
+            MoviesList moviesList = null;
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                Console.WriteLine(response.IsSuccessStatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    moviesList = JsonConvert.DeserializeObject<MoviesList>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\tERROR {0}", ex.Message);
+            }
+
+            return moviesList.ResultsArray;
+        }
+    }
+
+}
---
+++ AppSuggest/AppSuggest/Classes/MovieData.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSuggest
+{
+    public class MovieData
+    {
+        [JsonProperty("Title")]
+        public string Title { get; set; }
+
+        [JsonProperty("Poster")]
+        public string Poster { get; set; }
+
+        [JsonProperty("Director")]
+        public string Director { get; set; }
+
+        [JsonProperty("Year")]
+        public long Year { get; set; }
+    }
+}
