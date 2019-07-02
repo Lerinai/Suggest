ebf61c0c630ec80f355c3a49d252641a3f187fdb
FlowerSushi <luc.vincent531@hotmail.Fr>
2019-07-02 12:00:53 +0200

    Added Posters and genres to listview

---
+++ AppSuggest/AppSuggest/Classes/Genre.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSuggest
+{
+    public class Genre
+    {
+        [JsonProperty("id")]
+        public int ID { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        /*public async static Task<List<Genre>> GetGenres()
+        {
+            if (_genres == null)
+            {
+                RestService _restService = new RestService();
+                _genres = await _restService.GetGenresAsync();
+            }
+            return _genres;
+        }*/
+    }
+}
--- AppSuggest/AppSuggest/Classes/Movie.cs
+++ AppSuggest/AppSuggest/Classes/Movie.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Collections;
+
 
 namespace AppSuggest
 {
@@ -16,11 +19,45 @@ namespace AppSuggest
         [JsonProperty("release_date")]
         public string ReleaseDate { get; private set; }
 
-        public Movie(int id, string titre, string rd)
+        [JsonProperty("poster_path")]
+        private string _posterPath;
+
+        [JsonProperty("genre_ids")]
+        private List<int> _genres;
+
+        public string Genres
         {
-            ID = id;
-            Title = titre;
-            ReleaseDate = rd;
+            get
+            {
+                string str = "";
+                foreach (int id in _genres)
+                {
+                    str += id + ", ";
+                }
+                return str.Remove(str.Length-2);
+            }
         }
+            
+
+        /*public string Genres {
+            get
+            {
+                string genres = "";
+                foreach (int id in _genres)
+                {
+                    genres += (from genre in Research.Genres
+                    where id == genre.ID
+                    select genre.Name) + ", ";
+                }
+                return genres.Substring(0, genres.Length-2); 
+            }
+            private set { } }*/
+
+        public string PosterURL
+        {
+            get { return $"{Constants.PosterEndPoint}/w200/{ _posterPath}"; }
+            private set { }
+        }
+
     }
 }
