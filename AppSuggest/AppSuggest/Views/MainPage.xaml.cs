87b2b31b38ae33e9bfac36113e2cce4201e0c7c9
Lerinai <mantes.bastien@stagiairesifa.fr>
2019-07-02 16:10:48 +0200

    creaProfil

--- AppSuggest/AppSuggest/Views/MainPage.xaml.cs
+++ AppSuggest/AppSuggest/Views/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace AppSuggest
@@ -13,9 +15,52 @@ namespace AppSuggest
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private string ClientID = "457784411675175";
+        public string accessToken;
         public MainPage()
         {
             InitializeComponent();
+
+            var apiRequest = "https://www.facebook.com/v3.3/dialog/oauth?client_id=" + ClientID + "&response_type=token&redirect_uri=https://www.facebook.com/connect/login_success.html";
+
+            var webView = new WebView
+            {
+                Source = apiRequest,
+                HeightRequest = 1
+            };
+
+            webView.Navigated += WebViewOnNavigated;
+            Content = webView;
+        }
+
+        private async void WebViewOnNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            accessToken = ExtractAccessTokenFromUrl(e.Url);
+
+            if(accessToken != "")
+            {
+                await GetFacebookProfileAsync(accessToken);
+            }
+        }
+
+        private string ExtractAccessTokenFromUrl(string url)
+        {
+            string extractedToken = "";
+            if (url.Contains("access_token") && url.Contains("&expires_in"))
+            {
+                extractedToken = url.Replace("https://www.facebook.com/connect/login_success.html#access_token", "");
+                extractedToken = extractedToken.Replace("&expires_in", "");
+            }
+            return extractedToken;
+        }
+
+            public async Task GetFacebookProfileAsync(string accessToken)
+        {
+            var requestUrl = "https://graph.facebook.com/v2.7/me" + "?fields=name,picture,age_range,cover,devices,is_verified" + "&access_token=" + accessToken;
+
+            var httpClient = new HttpClient();
+
+            var userJson = await httpClient.GetStringAsync(requestUrl);
         }
     }
 }
