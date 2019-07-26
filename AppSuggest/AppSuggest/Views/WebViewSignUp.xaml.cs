781bc3f081686fdd0f02e8c1d5f9d081ba4255af
CapiCode <giovannikce@gmail.com>
2019-07-27 01:19:49 +0200

    Ajout webview/Sign Up > LoginPage

--- AppSuggest/AppSuggest/Views/WebViewSignUp.xaml.cs
+++ AppSuggest/AppSuggest/Views/WebViewSignUp.xaml.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AppSuggest.Classes;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AppSuggest.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class WelcomePage : ContentPage
+    public partial class WebViewSignUp : ContentPage
     {
-        public WelcomePage()
+        public WebViewSignUp()
         {
             InitializeComponent();
         }
-
-        private void Start(object sender, EventArgs e)
+        public WebViewSignUp(string url)
         {
-            Application.Current.MainPage = new MainPage();
+            InitializeComponent();
+
+            webview.Source = url;
         }
     }
 }
