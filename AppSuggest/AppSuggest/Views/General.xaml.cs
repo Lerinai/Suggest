088b7a9d3e87fff60c57b3884052318f960811c2
CapiCode <giovannikce@gmail.com>
2019-07-27 18:17:11 +0200

    Avancement Views/Setting 70%

--- AppSuggest/AppSuggest/Views/General.xaml.cs
+++ AppSuggest/AppSuggest/Views/General.xaml.cs
@@ -10,11 +10,36 @@ using Xamarin.Forms.Xaml;
 namespace AppSuggest.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    public class Option
+    {
+        public String Name { get; set; }
+        public String Description { get; set; }
+    }
     public partial class General : ContentPage
     {
+        //List<Option> options();
         public General()
         {
             InitializeComponent();
+            //options = new List<Option>();
+            //options.Add(new Option { Name = "Lait", Description = "Deux briques de lait" });
+            //options.Add(new Option { Name = "Canne à sucre", Description = "100% locale" });
+            //options.Add(new Option { Name = "Rhum-Vieux",  Description = "En tout honneur" });
+            //options.Add(new Option { Name = "Abricot", Description = "Pêché mignon" });
+
+            //GeneralOptions.ItemsSource = options;
+            //GeneralOptions.ItemSelected += (sender, e) =>
+            //{
+            //    if (GeneralOptions.SelectedItem != null)
+            //    {
+
+            //        Option item = GeneralOptions.SelectedItem as Option;
+
+            //        DisplayAlert(item.Name, item.Description, "OK");
+            //        GeneralOptions.SelectedItem = null;
+            //    }
+
+            }
+
         }
     }
-}
